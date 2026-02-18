0db0d91eac50eb15dfca325a6cfef7ee3fefcba4
harshal <harshalsomkuwar9@gmail.com>
2026-02-18 17:13:28 +0530

    Setuped NativeWind and navigation for the SWAP assignments

---
+++ day_2/swap/app/(tabs)/_layout.tsx
@@ -0,0 +1,34 @@
+import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+
+export default function TabLayout () {
+
+    return (
+
+        <Tabs screenOptions={{
+        headerShown: false,
+        tabBarStyle: {
+          backgroundColor: "#16161D",
+          borderTopColor: "#2A2A35",
+        },
+        tabBarActiveTintColor: "#14F195",
+        tabBarInactiveTintColor: "#6B7280",
+      }}>
+        
+        <Tabs.Screen 
+        name="index"
+        options={{
+          title: "Swap",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="swap-horizontal" size={size} color={color} />
+          ),
+        }}>
+
+        </Tabs.Screen>
+            
+
+        </Tabs>
+
+    )
+
+}
---
+++ day_2/swap/app/_layout.tsx
@@ -0,0 +1,15 @@
+import "../global.css"
+import { Stack } from "expo-router";
+import { StatusBar } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+export default function RootLayout() {
+  return (
+    <SafeAreaProvider>
+      <StatusBar hidden />
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="(tabs)" />
+      </Stack>
+    </SafeAreaProvider>
+  );
+}
